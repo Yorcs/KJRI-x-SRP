17c63239e23f4203bf6f4ff374c3f67fd0d64068
Kelly <75859041+kellyhady@users.noreply.github.com>
2023-08-30 12:41:43 -0700

    Update identity.dart

--- self_report_application/lib/identity.dart
+++ self_report_application/lib/identity.dart
@@ -6,19 +6,19 @@ import 'package:self_report_application/requirements_page.dart';
 //Requirements Page
 class IdentityPage extends StatelessWidget {
   const IdentityPage({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return IdentityForm();
   }
 }
 
-class IdentityForm extends StatefulWidget{
+class IdentityForm extends StatefulWidget {
   @override
   _IdentityFormState createState() => _IdentityFormState();
 }
 
-class _IdentityFormState extends State<IdentityForm>{
+class _IdentityFormState extends State<IdentityForm> {
   final identityKey = GlobalKey<FormState>();
 
   String? dropdownValue;
@@ -32,77 +32,71 @@ class _IdentityFormState extends State<IdentityForm>{
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
-          //TODO: Readjust margin and padding
-          body: Container(
-            child: Form(
-              key: identityKey,
-              child:Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TextsForm(labels: 'a'),
-                  TextFormField(
-                    decoration: InputDecoration(
+            //TODO: Readjust margin and padding
+            body: Container(
+          child: Form(
+            key: identityKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextsForm(labels: 'a'),
+                TextFormField(
+                  decoration: InputDecoration(
                       floatingLabelBehavior: FloatingLabelBehavior.always,
                       labelText: 'Tanggal Lahir',
-                      hintText: 'DD/MM/YYYY'
-                    ),
-                    validator:(value){
-                      if(value!.isEmpty || !RegExp(r'^[a-z A-Z]+$').hasMatch(value)){
-                        return "Invalid input"; //TODO: Please change prompt
-                      } else{
-                        return null;
-                      }
-                    },
-                  ),
-                  TextsForm(labels: 'aaaaaaaaaaaaaaaaaaaaaaaa'),
-                  TextFormField(
-                    decoration: InputDecoration(
+                      hintText: 'DD/MM/YYYY'),
+                  validator: (value) {
+                    if (value!.isEmpty ||
+                        !RegExp(r'^[a-z A-Z]+$').hasMatch(value)) {
+                      return "Invalid input"; //TODO: Please change prompt
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                TextsForm(labels: 'aaaaaaaaaaaaaaaaaaaaaaaa'),
+                TextFormField(
+                  decoration: InputDecoration(
                       floatingLabelBehavior: FloatingLabelBehavior.always,
-                      labelText: 'Nomor Paspor'
-                    ),
-                    validator:(value){
-                      if(value!.isEmpty || !RegExp(r'^[a-z A-Z 0-9]+$').hasMatch(value!)){
-                        return "Invalid input"; //TODO: Please change prompt
-                      } else{
-                        return null;
-                      }
-                    },
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
+                      labelText: 'Nomor Paspor'),
+                  validator: (value) {
+                    if (value!.isEmpty ||
+                        !RegExp(r'^[a-z A-Z 0-9]+$').hasMatch(value!)) {
+                      return "Invalid input"; //TODO: Please change prompt
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
                       floatingLabelBehavior: FloatingLabelBehavior.always,
-                      labelText: 'NIK'
-                    ),
-                    validator:(value){
-                      if(value!.isEmpty || !RegExp(r'^[0-9]+$').hasMatch(value!)){
-                        return "Invalid input"; //TODO: Please change prompt
-                      } else{
-                        return null;
-                      }
-                    },
-                  ),
-                  DropdownButton(
-                    value: dropdownValue,
-                    hint: Text('Jenis Kelamin'),
-                    items: items.map((String items){
-                      return DropdownMenuItem(
-                        value: items,
-                        child: Text(items)
-                      );
-                    }).toList(),
-
-                    onChanged: (String? newValue){
-                      setState(() {
-                        dropdownValue = newValue!;
-                      });
-                    },
-                  )
-
-                ],
-              ), 
+                      labelText: 'NIK'),
+                  validator: (value) {
+                    if (value!.isEmpty ||
+                        !RegExp(r'^[0-9]+$').hasMatch(value!)) {
+                      return "Invalid input"; //TODO: Please change prompt
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                DropdownButton(
+                  value: dropdownValue,
+                  hint: Text('Jenis Kelamin'),
+                  items: items.map((String items) {
+                    return DropdownMenuItem(value: items, child: Text(items));
+                  }).toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue = newValue!;
+                    });
+                  },
+                )
+              ],
             ),
-          )
-        );
+          ),
+        ));
       },
     );
   }
@@ -113,19 +107,17 @@ class TextsForm extends StatelessWidget {
     super.key,
     String? labels,
   });
-  
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       decoration: InputDecoration(
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        labelText: 'nama lengkap'
-      ),
-      validator:(value){
-        if(value!.isEmpty || !RegExp(r'^[a-z A-Z]+$').hasMatch(value)){
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          labelText: 'nama lengkap'),
+      validator: (value) {
+        if (value!.isEmpty || !RegExp(r'^[a-z A-Z]+$').hasMatch(value)) {
           return "Invalid input"; //TODO: Please change prompt
-        } else{
+        } else {
           return null;
         }
       },
