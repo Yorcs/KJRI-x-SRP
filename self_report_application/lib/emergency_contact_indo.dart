b4bc8eeac52ac8c8d2e69a9ff6233fc508b9a1b4
LightChase <83929087+Yorcs@users.noreply.github.com>
2024-09-10 00:08:47 -0700

    Removed unused import on emergency_contact_indo.dart

--- self_report_application/lib/emergency_contact_indo.dart
+++ self_report_application/lib/emergency_contact_indo.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:self_report_application/header.dart';
 import 'package:self_report_application/form_container.dart';
 import 'package:self_report_application/overview.dart';
